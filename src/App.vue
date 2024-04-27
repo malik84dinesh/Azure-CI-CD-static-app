f71dd841b0c36c0cdf7afa7e3ced4537289c8d0a
DineshMalik <dineshmalik007@gmail.com>
2024-04-27 22:50:03 +0530

    fixed AB#1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <h1>Your new LLM Model</h1>
-	<h2>Upload your data to make to get predictions</h2>
+	<h2>Upload your data to get gen AI predictions</h2>
     <img
       alt="Vue logo"
       style="width: 100px; height: 100px"
